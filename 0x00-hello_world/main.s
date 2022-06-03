1b87f9b8dd88ada0cd452898095e241f2a1f2bb0
natiyeshi <natiyeshi16@gmail.com>
2022-06-03 03:38:04 -0400

    D

---
+++ 0x00-hello_world/main.s
@@ -0,0 +1,30 @@
+	.file	"main.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+.LC0:
+	.string	"helllllo"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 11.2.0-13) 11.2.0"
+	.section	.note.GNU-stack,"",@progbits

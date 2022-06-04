69a32f0e390fe1ae78851ff8db0809e97e001db5
natiyeshi <natiyeshi16@gmail.com>
2022-06-04 07:12:08 -0400

    D

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - entry pint
+ * Return: return
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - entry pint
+ * Return: return
+ */
+
 int main(void)
 {
 	int INT;

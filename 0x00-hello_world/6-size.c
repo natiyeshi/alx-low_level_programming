03ee57323607ed0064f97a2fd14cd561efac0f08
natiyeshi <natiyeshi16@gmail.com>
2022-06-04 07:30:14 -0400

    D

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long int LINT;
 	long long int LLINT;
 
-	printf("Size of a char: %zu bite(s)\n", sizeof(CHAR));
-	printf("Size of an int: %zu bite(s)\n", sizeof(INT));
-	printf("Size of a long int: %zu bite(s)\n", sizeof(LINT));
-	printf("Size of a long long int: %zu bite(s)\n", sizeof(LLINT));
-	printf("Size of a float: %zu bite(s)\n", sizeof(FLOAT));
+	printf("Size of a char: %zu byte(s)\n", sizeof(CHAR));
+	printf("Size of an int: %zu byte(s)\n", sizeof(INT));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(LINT));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(LLINT));
+	printf("Size of a float: %zu byte(s)\n", sizeof(FLOAT));
 	return (0);
 }

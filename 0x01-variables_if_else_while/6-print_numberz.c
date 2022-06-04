1faf910a31c252a2f175b2396cf5b1a40d7bb5b8
natiyeshi <natiyeshi16@gmail.com>
2022-06-04 08:00:54 -0400

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - jsdlkf
+ * Return: re
+ */
+
+int main(void)
+{
+	char y = '0';
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(y);
+		y++;
+	}
+	printf("\n");
+
+	return (0);
+}
+

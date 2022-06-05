23957dda0097ff05d8d532870339e185048a26b1
natiyeshi <natiyeshi16@gmail.com>
2022-06-05 13:22:41 -0400

    d

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - m
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+	int temp;
+	int lela;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		for (temp = '0'; temp <= '9'; temp++)
+		{
+			for (lela = '0'; lela <= '9'; lela++)
+			{
+				putchar(ch);
+				putchar(temp);
+				putchar(' ');
+				putchar(temp);
+				putchar(lela);
+				if(ch =='9' && temp == '9' && lela == '9')
+					continue;
+				putchar(',');
+			}
+		}
+	}
+	printf("\n");
+
+	return (0);
+}

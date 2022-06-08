fe355078bc355d0bc8799c479c077f051436b3dc
natiyeshi <natiyeshi16@gmail.com>
2022-06-08 07:40:10 -0400

    d

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,29 +1,5 @@
 #include <stdio.h>
 #include <ctype.h>
-/**
- * main - d
- * Return: 0
- */
-
-/**
- *@c: fils
- */
-int _islower(int c);
-
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-
-	return (0);
-}
 
 /**
  * _islower - file

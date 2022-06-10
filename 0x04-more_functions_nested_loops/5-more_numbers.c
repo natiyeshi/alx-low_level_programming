9ffd38b303f96eca30c66fd098597c766ef78428
natiyeshi <natiyeshi16@gmail.com>
2022-06-10 04:58:50 -0400

    d

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - dfads
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (y = 0; y < 10; x++)
+	{
+		for (x = 0; x < 15; x++)
+			printf("%d", x);
+		printf("\n");
+	}

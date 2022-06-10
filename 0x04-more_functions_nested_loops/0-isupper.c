b94673ba879a8220139bcac7cab3fc3f653ea3e6
natiyeshi <natiyeshi16@gmail.com>
2022-06-10 15:21:17 -0400

    d

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * _isupper - nati
- * @c: nata
+ * _isupper - checks
+ * @c: variable
  * Return: 0
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}

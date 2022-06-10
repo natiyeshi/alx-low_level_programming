b3d011137455e91b452a4a3298e9a755797fc468
natiyeshi <natiyeshi16@gmail.com>
2022-06-10 16:07:35 -0400

    d

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,44 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - f
- * Return: 0
- */
-
+  * main - Prints a Fizz Buzz program
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	int x;
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 5 == 0 && x % 3 == 0)
-			printf("FizzBuzz ");
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
 		else if (x % 3 == 0)
-			printf("Fizz ");
+		{
+			printf("Fizz");
+		}
 		else if (x % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("Buzz");
+		}
 		else
-			printf("%d ", x);
+		{
+			printf("%d", x);
+		}
+
+		if (x != 100)
+		{
+			printf(" ");
+		}
 	}
+
 	printf("\n");
 
 	return (0);
 }
+© 2022 GitHub, Inc.
+Terms
+Privac

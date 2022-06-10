bd3876fb8e20e2be7813c0bda693d2206ba3aa8b
natiyeshi <natiyeshi16@gmail.com>
2022-06-10 05:52:48 -0400

    d

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - f
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 1; x <= 100; x++)
+	{
+
+		if (x % 5 == 0 && x % 3 == 0)
+			printf("FizzBuzz");
+		else if (x % 3 == 0)
+			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", x);
+		printf(" ");
+	}
+
+	return (0);
+}

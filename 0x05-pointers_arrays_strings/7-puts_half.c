2bb0a18a5a8f60cba9823521f8ace57e16bd3783
natiyeshi <natiyeshi16@gmail.com>
2022-06-13 16:29:15 -0400

    m

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - file
+ * @str: str
+ */
+void puts_half(char *str)
+{
+	int i, length;
+
+	length = strlen(str) % 2 == 0 ? strlen(str) / 2 : (strlen(str) - 1) / 2;
+	for (i = 0; i < length; i++)
+		printf("%c", str[i]);
+	printf("\n");
+}

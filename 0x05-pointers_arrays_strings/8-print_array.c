dc5de21cf86afbe863fd1960ac6f3b102d8d2750
natiyeshi <natiyeshi16@gmail.com>
2022-06-13 16:39:20 -0400

    m

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "mian.h"
+
+/**
+ * print_array - file
+ * @a: file
+ * @n: num
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < 5 ; i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+	printf("%d");
+}

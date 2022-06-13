26072e68148ddb796ed62c81ef1bc280d119cb9e
natiyeshi <natiyeshi16@gmail.com>
2022-06-13 15:48:02 -0400

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * print_rev - file
  * @s: point
@@ -11,8 +11,8 @@ void print_rev(char *s)
 	char temp;
 	int i, length;
 
-	length = sizeof(s) - 1;
-	for (i = 0; i < sizeof(s) / 2; i++)
+	length = strlen(s) - 1;
+	for (i = 0; i < strlen(s) / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[length];
